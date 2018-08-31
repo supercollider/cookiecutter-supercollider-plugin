66ad146124145a2f68d2cc40cf2d0c0857fbd859
Brian Heim <brianlheim@gmail.com>
2018-08-30 22:30:33 -0500

    import os in script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,7 @@
 '''Init git repository and make initial commit.'''
 
 from subprocess import check_call
+import os
 
 git_url = 'git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}'
 sc_path = '{{ cookiecutter.full_path_to_supercollider_source }}'
